6c1a033ee890b9050bcf5371f261ddc4d105b3bd
Sean McKinney <seanamckinney8@gmail.com>
2022-06-13 14:20:01 -0500

    Add project files.

---
+++ HFCSPg417StoreShoesApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HFCSPg417StoreShoesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
